116de11275d65a9c4eefe9fc3b7883d29067b693
Tommy Webb <tommygeorgewebb@googlemail.com>
2021-04-01 14:21:41 +0100

    Fixed bot not responding to messages

--- commands.js
+++ commands.js
@@ -1,20 +1,23 @@
 //const fetch = require ("node-fetch");
 const kills = require("./commands/kills.js");
 const deaths = require("./commands/deaths.js");
+const overview = require("./commands/overview.js");
 
-const commands = { kills, deaths };
+const commands = { kills, deaths, overview };
 
 module.exports = async function (msg) {
-    if(msg.channel.id == '827125983173672961') {
-        let tokens = msg.content.split(" ");
-        let command = tokens.shift();
+    if(msg.channel.id == "293833885270409216" || msg.channel.id == "827125983173672961"){
+    let tokens = msg.content.split(" ");
+    let command = tokens.shift();
 
-        if(command.substring(0) === "*r6") {
-            command = tokens[1];
-            commands[command](msg, tokens);
-        }
-            //msg.reply("Very Nice");
-
+    if(command.substring(0) === "*r6") {
+        command = tokens[1];
+        commands[command](msg, tokens);
     }
+        //msg.reply("Very Nice");
+    }
+
+
+    
 
 }; 
--- commands/speech.js
+++
@@ -1,9 +0,0 @@
-const replies = [
-    "wow",
-    "great",
-    "cool",
-    "Very Nice"
-]
-
-const index = Math.floor(Math.random()*replies.length);
-msg.channel.send(replies[index]);
